fb269dbd886dfd371a73f5a7ea9328dff85769cd
Martin <nomonoboa@gmail.com>
2023-01-10 13:00:00 -0600

    Add tarea1

---
+++ Tarea1/tarea.py
@@ -0,0 +1,15 @@
+# 
+# Sistemas Inteligentes
+# Aplicación de un perceptrón
+# @author:
+#       Valeria Guerra
+#       Emilio Rivas
+#       Nahim Medellín Torres
+#       Martin Noboa
+#       Emmanuel Bautista
+# @date: 
+#       Enero 10, 2023
+# @last_modified:
+#       Enero 10, 2023
+#
+
